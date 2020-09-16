bb77805b8dc4707dc8f2a3e1f3109e2ac87a5f45
glensun <glensun@tencent.com>
2020-09-16 19:44:21 +0800

    添加了几个方法

--- livedataext/src/main/java/com/glensun/livedataextension/LiveDataExt.kt
+++ livedataext/src/main/java/com/glensun/livedataextension/LiveDataExt.kt
@@ -878,42 +878,48 @@ inline fun <X> LiveData<X>.toMutable(): MutableLiveData<X> {
         return this
     } else {
         val result = MediatorLiveData<X>()
-        result.addSource(this, object : Observer<X> {
-            override fun onChanged(x: X) {
-                result.postValue(x)
-            }
-        })
+        result.addSource(this) { x -> result.postValue(x) }
         return result
     }
 }
 
-
-// 延迟发射, 注意LiveData的特点，如果lifecycle的生命周期没走到START，那是不会发射的
-fun <X> LiveData<X>.delay(milliseconds: Long): LiveData<X> {
+@MainThread
+inline fun <X> LiveData<X>.log(crossinline printer: (X?) -> Unit): LiveData<X> {
     val result = MediatorLiveData<X>()
-    result.addSource(this, object : Observer<X> {
-        override fun onChanged(x: X) {
-            Handler(Looper.getMainLooper())
-                .postDelayed(
-                    { result.postValue(x) },
-                    milliseconds
-                )
-        }
-    })
+    result.addSource(this) { x ->
+        printer(x)
+        result.setValue(x)
+    }
     return result
 }
 
-// 延迟发射, 注意LiveData的特点，如果lifecycle的生命周期没走到START，那是不会发射的
-// 另外，这个是MutableLiveData, 会使用原LiveData进行发射
-fun <X> MutableLiveData<X>.delay(milliseconds: Long): MutableLiveData<Pair<Boolean, X?>> {
-    val result = MediatorLiveData<Pair<Boolean, X?>>()
-    Handler(Looper.getMainLooper())
-        .postDelayed({
-            result.postValue(Pair(true, this.value))
-        }, milliseconds)
-    result.postValue(Pair(false, this.value))
-    return result
-}
+//
+//// 延迟发射, 注意LiveData的特点，如果lifecycle的生命周期没走到START，那是不会发射的
+//fun <X> LiveData<X>.delay(milliseconds: Long): LiveData<X> {
+//    val result = MediatorLiveData<X>()
+//    result.addSource(this, object : Observer<X> {
+//        override fun onChanged(x: X) {
+//            Handler(Looper.getMainLooper())
+//                .postDelayed(
+//                    { result.postValue(x) },
+//                    milliseconds
+//                )
+//        }
+//    })
+//    return result
+//}
+//
+//// 延迟发射, 注意LiveData的特点，如果lifecycle的生命周期没走到START，那是不会发射的
+//// 另外，这个是MutableLiveData, 会使用原LiveData进行发射
+//fun <X> MutableLiveData<X>.delay(milliseconds: Long): MutableLiveData<Pair<Boolean, X?>> {
+//    val result = MediatorLiveData<Pair<Boolean, X?>>()
+//    Handler(Looper.getMainLooper())
+//        .postDelayed({
+//            result.postValue(Pair(true, this.value))
+//        }, milliseconds)
+//    result.postValue(Pair(false, this.value))
+//    return result
+//}
 
 
 //fun <X> MutableLiveData<X>.suspendDelay(milliseconds: Long, block: MutableLiveData<X>.() -> Unit): MutableLiveData<X> {
@@ -929,93 +935,4 @@ fun <X> MutableLiveData<X>.delay(milliseconds: Long): MutableLiveData<Pair<Boole
 //                block()
 //            }, milliseconds)
 //    return this
-//}
-// 转变成mutableLiveData
-fun <X> LiveData<X>.toMutableAndEmit(): MutableLiveData<X> {
-    if (this is MutableLiveData) {
-        this.postValue(this.value)
-        return this
-    } else {
-        val result = MediatorLiveData<X>()
-        result.addSource(this, object : Observer<X> {
-            override fun onChanged(x: X) {
-                result.postValue(x)
-            }
-        })
-        result.postValue(this.value)
-        return result
-    }
-}
-
-// 发射一个元素
-fun <X> MutableLiveData<X>.emitter(block: () -> X): MutableLiveData<X> {
-    this.postValue(block())
-    return this
-}
-
-
-// 打个log
-fun <X> LiveData<X>.log(printer: (X) -> Unit): LiveData<X> {
-    val result = MediatorLiveData<X>()
-    result.addSource(this, object : Observer<X> {
-        override fun onChanged(x: X) {
-            printer(x)
-            result.postValue(x)
-        }
-    })
-    return result
-}
-
-// 对于一个Boolean类型的LiveData，判断其值是否为真
-fun LiveData<Boolean>.isTrue(): Boolean {
-    return this.value == true
-}
-
-//inline fun <X, Y> LiveData<X>.switchMap(
-//    crossinline transform: (X?) -> LiveData<Y>
-//): LiveData<Y> = Transformations.switchMap(this) { transform(it) }
-//
-//inline fun <X> LiveData<X>.distinctUntilChanged(): LiveData<X> =
-//    Transformations.distinctUntilChanged(this)
-
-//
-//fun <T> merge(vararg liveDatas: LiveData<T>): LiveData<List<T?>> {
-//    return merge(liveDatas.toList())
-//}
-//
-//fun <T> merge(liveDatas: List<LiveData<T>>): LiveData<List<T?>> {
-//    val result = MediatorLiveData<List<T?>>()
-//
-//    liveDatas.forEachIndexed { index, liveData ->
-//        result.addSource(liveData) {
-//            var resultList = result.value?.toMutableList()
-//            if (resultList == null) {
-//                resultList = MutableList(liveDatas.size) { null }
-//            }
-//            resultList[index] = it
-//
-//            result.postValue(resultList)
-//        }
-//    }
-//    return result
-//}
-//
-//fun <T> LiveData<List<T?>>.merge(liveData: LiveData<T>): LiveData<List<T?>> {
-//    val result = MediatorLiveData<List<T?>>()
-//    result.addSource(this, object : Observer<List<T?>> {
-//        override fun onChanged(it: List<T?>?) {
-//            it?.toMutableList()?.add(liveData.value)
-//            result.postValue(it)
-//        }
-//    }
-//    )
-//    result.addSource(liveData, object : Observer<T> {
-//        override fun onChanged(it: T) {
-//            val list = this@merge.value
-//            list?.toMutableList()?.add(it)
-//            result.postValue(list)
-//        }
-//
-//    })
-//    return result
 //}
